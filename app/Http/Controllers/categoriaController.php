239afe73329a300ba92935eaa41e09343d2de83b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-01 21:41:13 -0500

    integrando el script para hacer el listado de los registros realizados de categoria y realizando el método del registro de categorias

--- app/Http/Controllers/categoriaController.php
+++ app/Http/Controllers/categoriaController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoriaRequest;
+use App\Models\Caracteristica;
+use App\Models\Categoria;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class categoriaController extends Controller
 {
@@ -13,7 +18,9 @@ class categoriaController extends Controller
      */
     public function index()
     {
-        return view('categoria.index');
+        /*TODO: se utiliza en este caso el with para obtener el id de caracteristica */
+        $categorias = Categoria::with('caracteristica')->get();
+        return view('categoria.index', ['categorias' => $categorias]);
     }
 
     /**
@@ -23,7 +30,7 @@ class categoriaController extends Controller
      */
     public function create()
     {
-        //
+        return view('categoria.create');
     }
 
     /**
@@ -31,10 +38,23 @@ class categoriaController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
+     * 
+     * TODO: mediante el comando php artisan make:request StoreCategoriaRequest se crea la clase StoreCategoriaRequest
+     * con la cual se podrán realizar las validaciones de los campos de texto del formulario
      */
-    public function store(Request $request)
+    public function store(StoreCategoriaRequest $request)
     {
-        //
+        try {
+            DB::beginTransaction();                                          /* Inicio de la transacción */
+            $caracteristica = Caracteristica::create($request->validated());
+            $caracteristica->categoria()->create([
+                'caracteristica_id' => $caracteristica->id
+            ]);
+            DB::commit();                                                       /* si todo resulta correcto se hace el commit  */
+        } catch (Exception $e) {
+            DB::rollBack();                                                     /* si no esta correcto el procedimiento de la consulta se revierte todo */
+        }
+        return redirect()->route('categorias.index')->with('success','Categoría Registrada');
     }
 
     /**
