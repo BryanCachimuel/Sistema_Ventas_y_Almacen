c58f7250bc9f166868d801f7751bc9ed4d71d3be
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-04 21:58:30 -0500

    ingresando una nueva ruta para acceder a los métodos del controlador de presentaciones

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\categoriaController;
+use App\Http\Controllers\presentacioneController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,8 @@ Route::get('/', function () {
 Route::view('/panel', 'panel.index')->name('panel');
 
 Route::resource('categorias', categoriaController::class);
+
+Route::resource('presentaciones', presentacioneController::class);
 
 Route::get('/login', function () {
     return view('auth/login');
