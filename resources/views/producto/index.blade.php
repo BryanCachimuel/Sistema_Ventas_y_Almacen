2976c845d680d345b70de9c94b25b04a876e311d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-18 22:14:27 -0500

    realizando el listado de los productos en una tabla y añadiendo sus respectivos botones de funciones como editar, ver y eliminar

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -8,6 +8,21 @@
 @endpush
 
 @section('content')
+
+<!-- dentro del método store se envia un mensaje con la clave success -->
+@if (session('success'))
+<script>
+    let message = "{{ session('success') }}"
+
+    Swal.fire({
+        icon: "success",
+        title: message,
+        showConfirmButton: false,
+        timer: 1500
+    });
+</script>
+@endif
+
 <div class="container-fluid px-4">
     <h1 class="mt-4 text-center">Productos</h1>
     <ol class="breadcrumb mb-4">
@@ -32,14 +47,89 @@
             <table id="datatablesSimple" class="table table-striped text-center">
                 <thead>
                     <tr>
+                        <th>Código</th>
                         <th>Nombre</th>
-                        <th>Descripciones</th>
+                        <th>Marca</th>
+                        <th>Presentación</th>
+                        <th>Categorias</th>
                         <th>Estado</th>
                         <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-              
+                    @foreach ($productos as $producto)
+                        <tr>
+                            <td>{{$producto->codigo}}</td>
+                            <td>{{$producto->nombre}}</td>
+                            <td>{{$producto->marca->caracteristica->nombre}}</td>
+                            <td>{{$producto-presentacione->caracteristica->nombre>}}</td>
+                            <td>
+                                @foreach ($producto->categoria as $ctg)
+                                    <div class="container">
+                                        <div class="row">
+                                            <span class="m-1 rounded-pill p-1 bg-secondary text-white text-center">{{$ctg->caracteristica->nombre}}</span>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </td>
+                            <td>
+                                @if ($producto->estado == 1)
+                                    <span class="fw-bolder rounded p-1 bg-success text-white">Activo</span>
+                                @else
+                                <span class="fw-bolder rounded p-1 bg-danger text-white">Eliminado</span>
+                                @endif
+                            </td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                    <button type="button" class="btn btn-warning">Editar</button>
+                                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#verModal-{{$producto->id}}">Ver</button>
+                                    <button type="button" class="btn btn-danger">Eliminar</button>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <!-- Modal para ver -->
+                        <div class="modal fade" id="verModal-{{$producto->id}}" tabindex="-1" aria-labelledby="verModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Detalles del Producto</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                        <div class="modal-body">
+
+                                            <div class="row mb-3">
+                                                <label for="descripcion" class="form-label"> <span class="fw-bolder">Descripcion:</span> {{$producto->descripcion}}</label>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="fecha_vencimiento" class="form-label"><span class="fw-bolder">Fecha de Vencimiento:{{$producto->fecha_vencimiento =='' ? 'No tiene' : $producto->fecha_vencimiento}} </span></label>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="stock" class="form-label"><span class="fw-bolder">Stock: {{$producto->stock}}</span></label>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="img_path" class="form-label">Imagen: </label>
+                                                <div>
+                                                    @if ($producto->img_path != null)
+                                                        <img src="{{Storage::url(''.$producto->img_path)}}" alt="">
+                                                    @else
+                                                        
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+                    @endforeach
                 </tbody>
             </table>
         </div>
