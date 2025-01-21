c8777292e912aeea74077046d3222e40db563aeb
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-20 23:13:27 -0500

    Creando el archivo app.php donde se defne la url, name y el nombre de la sessión para el proyecto, como también se define la zona horaria respectiva con la que va a funcionar el proyecto

---
+++ bebidas/config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+    const APP_URL="http://localhost/sistemaBebidas/bebidas/";
+    const APP_NAME="RIXLER BEBIDAS";
+    const APP_SESSION_NAME="RIXLER";
+
+    date_default_timezone_set("America/Guayaquil");
