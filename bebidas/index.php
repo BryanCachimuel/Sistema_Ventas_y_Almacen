a012ef465ee3ae671141e229deaa4e58de52f48d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-21 21:13:48 -0500

    Realizando una validación para las rutas que se visiten y separandolas adecuadamente para ubicarlas en un arreglo y también realizando el procedimiento de preparación de la plantilla base del sistema

--- bebidas/index.php
+++ bebidas/index.php
@@ -1,5 +1,28 @@
 <?php
     require_once "./config/app.php";
-    
-    // manda a llamar a los archivos de clase que va a tener esté sistema 
-    require_once "./autoload.php";
+
+    require_once "./autoload.php";          // manda a llamar a los archivos de clase que va a tener esté sistema 
+
+    if(isset($_GET['views'])){
+        
+       $url = explode("/",$_GET['views']);       // se separán los valores de las urls del proyecto
+       
+       
+    }else{
+       
+        $url = ["login"];
+
+    }
+   
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php require_once "./app/views/inc/head.php"; ?>
+</head>
+<body>
+
+    <?php require_once "./app/views/inc/script.php"; ?>
+</body>
+</html>
