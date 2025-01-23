1b8bb35a3a3724c95ab52419298e7bd9f3754ae5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-22 21:15:43 -0500

    Invocación al controlador de la vista para poder trabajar con la url y poder mostrar las vistas respectivas manejadas desde el controlador

--- bebidas/index.php
+++ bebidas/index.php
@@ -3,6 +3,8 @@
 
     require_once "./autoload.php";          // manda a llamar a los archivos de clase que va a tener esté sistema 
 
+    require_once "./app/views/inc/session_start.php";    // archivo de inicio de sesión 
+    
     if(isset($_GET['views'])){
         
        $url = explode("/",$_GET['views']);       // se separán los valores de las urls del proyecto
@@ -23,6 +25,21 @@
 </head>
 <body>
 
-    <?php require_once "./app/views/inc/script.php"; ?>
+    <?php 
+        use app\controllers\viewsController;
+
+        $viewsController = new viewsController(); // se instancia la clase para poder tener acceso a sus métodos
+        $vista = $viewsController->obtenerVistasControlador($url[0]);   // accede a la posición cero de la url del navegador
+
+        if($vista == "login" || $vista == "404"){
+           require_once "./app/views/content/".$vista."-view.php";
+        } 
+        else{
+            require_once $vista;
+        }
+        
+
+        require_once "./app/views/inc/script.php"; 
+    ?>
 </body>
 </html>
