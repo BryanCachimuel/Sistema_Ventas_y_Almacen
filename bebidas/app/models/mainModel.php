0cd2f79d94959c34fc8ca3173fb364b79dcf0111
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-23 19:33:41 -0500

    Realizando la función para la conexión hacia la base de datos

--- bebidas/app/models/mainModel.php
+++ bebidas/app/models/mainModel.php
@@ -1,6 +1,7 @@
 <?php
 
     namespace app\models;
+    use \PDO;            // le decimos al modelo que debe usar para la conexión a la bdd la clase PDO
 
     if(file_exists(__DIR__."/../../config/server.php")){
         require_once __DIR__."/../../config/server.php";
@@ -12,5 +13,11 @@
         private $db = DB_NAME;
         private $user = DB_USER;
         private $pass = DB_PASS;
+
+        protected function conectar(){
+            $conexion = new PDO("mysql:host=".$this->server.";dbname=".$this->db, $this->user, $this->pass);
+            $conexion->exec("SET CHARACTER SET utf8");          // le decimos a que solo se van a utilizar caracteres de tipo utf8
+            return $conexion;
+        }
 
     }
