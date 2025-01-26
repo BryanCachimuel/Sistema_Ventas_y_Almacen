2638b8be523fab3b472d1aa246b0e8b1924f827c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-25 22:07:39 -0500

    Creando una función en el controlador para el registro de usuarios

--- bebidas/app/controllers/userController.php
+++ bebidas/app/controllers/userController.php
@@ -4,4 +4,31 @@
 
     class userController extends mainModel {
         
+        // Controlador para registrar un usuario
+        public function registrarUsuarioControlador(){
+
+            // Almacenando datos
+            $nombre = $this->limpiarCadena($_POST['usuario_nombre']);
+            $apellido = $this->limpiarCadena($_POST['usuario_apellido']);
+
+            $usuario = $this->limpiarCadena($_POST['usuario_usuario']);
+            $email = $this->limpiarCadena($_POST['usuario_email']);
+            $clave1 = $this->limpiarCadena($_POST['usuario_clave_1']);
+            $clave2 = $this->limpiarCadena($_POST['usuario_clave_2']);
+
+            // verificando campos obligatorios
+            if($nombre == "" || $apellido == "" || $usuario == "" || $clave1 == "" || $clave2 == ""){
+                $alerta = [
+                    "tipo" => "simple",
+                    "titulo" => "Ocurrio un error inesperado",
+                    "texto" => "No has llenado todos los campos que son obligatorios",
+                    "icono" => "error"
+                ];
+
+                return json_encode($alerta);
+                exit();
+            }
+            
+        }
+
     }
