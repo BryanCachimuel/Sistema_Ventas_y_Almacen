43046cb5ebac27c5f70b20e9d669af073ef59e66
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-27 22:34:19 -0500

    Realizando una función iniciarSesionControlador donde se hacen las validaciones de los inputs de usuario y clave para el proceso de inicio de sesión

--- bebidas/app/controllers/loginController.php
+++ bebidas/app/controllers/loginController.php
@@ -1,8 +1,63 @@
 <?php
 
 namespace app\controllers;
+
 use app\models\mainModel;
 
-class loginController extends mainModel {
-    
+class loginController extends mainModel
+{
+
+    //controlador para inicio de sesión
+    public function iniciarSesionControlador()
+    {
+
+        // Almacenando datos
+        $usuario = $this->limpiarCadena($_POST['login_usuario']);
+        $clave = $this->limpiarCadena($_POST['login_clave']);
+
+        if ($usuario == "" || $clave == "") {
+            echo "
+                <script>
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Ocurrio un error inesperado',
+                        text: 'No ha llenado todos los campos que son obligatorios',
+                        confirmButtonText: 'Aceptar',
+                    });
+                </script>
+            ";
+        }
+        else{
+            // verificando la integridad de los datos
+            if($this->verificarDatos("[a-zA-Z0-9]{4,20}", $usuario)){
+                echo "
+                    <script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Ocurrio un error inesperado',
+                            text: 'El Usuario no coincide con el formato solicitado',
+                            confirmButtonText: 'Aceptar',
+                        });
+                    </script>
+                ";
+            }
+            else{
+                if($this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}", $clave)){
+                    echo "
+                    <script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Ocurrio un error inesperado',
+                            text: 'La Clave no coincide con el formato solicitado',
+                            confirmButtonText: 'Aceptar',
+                        });
+                    </script>
+                ";
+                }
+                else{
+
+                }
+            }
+        }
+    }
 }
