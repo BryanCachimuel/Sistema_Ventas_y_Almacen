cb6dc034b3bc5165e6ab2eaa36e5248c42e76b81
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-06 21:43:46 -0500

    Creando el método logear el cual contiene validaciones para poder iniciar sesión con las credenciales de los usuarios que se han registrado en el sistema

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function index(){
-        return view("modules.auth.login");
+        $titulo = "Login de Usuario";
+        return view("modules.auth.login", compact("titulo"));
+    }
+
+    public function logear(Request $request){
+        // validar datos de las credenciales
+        $credenciales = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // buscar si el correo existe en la base de datos
+        $usuario = User::where('email', $request->email)->first();
+
+        // validar usuario y contraseña
+        if(!$usuario || !Hash::check($request->password, $usuario->password)){
+            return back()->withErrors(['email' => 'Credencial Incorrecta'])->withInput();
+        }
+
+        // validar si el usuario está activo
+        if($usuario->activo){
+            return back()->withErrors(['email' => 'Tu cuenta está inactiva']);
+        }
+
+        // crear la sesión de usuario
+        Auth::login($usuario);
+        $request->session()->regenerate();
+
+        return to_route('home');
     }
 }
