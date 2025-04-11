913469ee84414343f396908a177e6b0547fabddb
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-10 21:52:51 -0500

    Creando el modelo Categoria para que haga referencia a la tabla categorias de la base de datos

---
+++ app/Models/Categoria.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    // se asegura que este modelo trabaje con la tabla categorias
+    protected $table = 'categorias';
+}
