4cbdab23b10092d98b8007c66edfe24dc44cbc0d
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-10 21:56:23 -0500

    Mejorando la tabla donde se lista todas las categorías

--- resources/views/modules/categorias/index.blade.php
+++ resources/views/modules/categorias/index.blade.php
@@ -1,13 +1,58 @@
 @extends('layouts.main')
 
+@section('titulo', $titulo)
+    
+@section('contenido')
 <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Categorias</h1>
-    </div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Categorias</h1>
+  </div><!-- End Page Title -->
 
-    <section class="section dashboard">
-     
-    </section>
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Administrar Categorías</h5>
+            <p>Administrar las categorías de los productos</p>
+
+            <button class="btn btn-primary">
+              <i class='bx bxs-plus-circle'></i> Agregar Nueva Categoría
+            </button>
+            <hr>
+            <!-- Table with stripped rows -->
+            <table class="table datatable">
+              <thead>
+                <tr>
+                 <th>Nombre Categoría</th>
+                 <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($items as $item)
+                <tr>
+                  <td>
+                    {{ $item->nombre }}
+                  </td>
+                  <td>
+                    <a href="" class="btn btn-warning"><i class='bx bxs-edit'></i></a>
+                    <a href="" class="btn btn-danger"><i class='bx bxs-trash'></i></a>
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 
 </main>
+@endsection
+
