be06193378fcba99c6792b34eaaf033c9fa777d8
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-18 22:17:44 -0500

    Realizando joins con las entidades categorias y proveedores para obtener sus datos y mkostrarlos en la tabla de listado de productos

--- app/Http/Controllers/Productos.php
+++ app/Http/Controllers/Productos.php
@@ -17,7 +17,16 @@ class Productos extends Controller
     public function index()
     {
         $titulo = "Productos";
-        return view('modules.productos.index', compact('titulo'));
+        $items = Producto::select(
+            'productos.*',
+            'categorias.nombre as nombre_categoria',
+            'proveedores.nombre as nombre_proveedor'
+        )
+        ->join('categorias', 'productos.categoria_id', '=' , 'categorias.id')
+        ->join('proveedores', 'productos.proveedor_id', '=' , 'proveedores.id')
+        ->get();
+
+        return view('modules.productos.index', compact('titulo', 'items'));
     }
 
     /**
