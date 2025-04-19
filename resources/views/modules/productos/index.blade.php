e4c40b6b1d104e75bc22541b55a3a5bb1efc9988
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-18 20:56:44 -0500

    Agregando la vista que va a tener la parte del módulo de productos

--- resources/views/modules/productos/index.blade.php
+++ resources/views/modules/productos/index.blade.php
@@ -1,14 +1,79 @@
 @extends('layouts.main')
 
-
+@section('titulo', $titulo)
+    
+@section('contenido')
 <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Productos</h1>
-    </div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Productos</h1>
+  </div><!-- End Page Title -->
 
-    <section class="section dashboard">
-     
-    </section>
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Administrar Productos y Stock</h5>
+            <p>Administrar el Stock del Sistema</p>
+            <p>
+              <a href="" class="btn btn-success"> 
+                <i class="fa-solid fa-chart-line"></i> Productos con Stock Mínimo
+              </a>
+            </p>
+            <hr>
+            <a href="" class="btn btn-primary">
+              <i class='bx bxs-plus-circle'></i> Crear Producto
+            </a>
+            <hr>
+            <!-- Table with stripped rows -->
+            <table class="table table-striped">
+              <thead>
+                <tr class="text-center">
+                 <th>Categoría</th>
+                 <th>Proveedor</th>
+                 <th>Nombre</th>
+                 <th>Imagen</th>
+                 <th>Descripción</th>
+                 <th>Cantidad</th>
+                 <th>Venta</th>
+                 <th>Compra</th>
+                 <th>Activo</th>
+                 <th>Comprar</th>
+                 <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <tr class="text-center">
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <a href="" class="btn btn-warning"><i class='bx bxs-edit'></i></a>
+                    <a href="" class="btn btn-danger"><i class='bx bxs-trash'></i></a>
+                  </td>
+                </tr>
+  
+              </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 
 </main>
+@endsection
+
