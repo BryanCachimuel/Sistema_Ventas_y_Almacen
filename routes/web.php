cf6c64dad4a7f8b121315a798e2c3e038f4ff2c3
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-19 22:51:13 -0500

    Agregando las rutas show y destroy para poder ver producto que se va a eliminar

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::prefix('productos')->middleware("auth")->group(function(){
     Route::post('/store', [Productos::class, 'store'])->name('productos.store');
     Route::get('/edit/{id}', [Productos::class, 'edit'])->name('productos.edit');
     Route::put('/update/{id}', [Productos::class, 'update'])->name('productos.update');
+    Route::get('/show/{id}', [Productos::class, 'show'])->name('productos.show');
+    Route::delete('/destroy/{id}', [Productos::class, 'destroy'])->name('productos.destroy');
 });
 
 // Prefijo para todas las rutas que tengan que ver con clientes
