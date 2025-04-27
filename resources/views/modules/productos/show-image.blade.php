f5d35631f0aaa4051d481a42917d03e8dfcc86cb
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-26 23:01:00 -0500

    Creando la vista para poder actualizar la imagen del producto

---
+++ resources/views/modules/productos/show-image.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.main')
+
+@section('titulo', $titulo)
+    
+@section('contenido')
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h1>Editar Imagen de Producto</h1>
+  </div><!-- End Page Title -->
+
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Editar imagen de Producto del Stock</h5>
+
+            <hr>
+            <form action="{{ route('productos.update.image', $item->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+
+                <label for="archivo">Selecciona la nueva imagen</label>
+                <input type="file" name="archivo" id="archivo" class="form-control">
+
+               <div class="mt-3">
+                <button class="btn btn-warning">Actualizar Imagen</button>
+                <a href="{{ route("productos") }}" class="btn btn-info">Cancelar</a>
+               </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</main>
+@endsection
+
