c5d37ae6f71bc572d0aa5142b2a8002e0216175e
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-26 23:29:03 -0500

    Actualizando el mensaje de error para las consultas hacia la base de datos

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -88,7 +88,7 @@
 
     @if(session('error'))
       Swal.fire({
-        title: 'Exito',
+        title: 'Error',
         text: '{{session('success')}}',
         icon:'error',
         confirmButtonText: 'Aceptar'
