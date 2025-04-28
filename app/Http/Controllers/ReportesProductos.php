3fdb153fe176e0d933b89f5ef6a061aaf7e750ac
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-27 21:51:40 -0500

    Realizando el script de la consulta hacia la base de datos para obetner todos los registros de la base de datos para mostrarlos en la vista de reportes

--- app/Http/Controllers/ReportesProductos.php
+++ app/Http/Controllers/ReportesProductos.php
@@ -13,10 +13,13 @@ class ReportesProductos extends Controller
         $items = Producto::select(
             'productos.*',
             'categorias.nombre as nombre_categoria',
-            'proveedores.nombre as nombre_proveedor'
+            'proveedores.nombre as nombre_proveedor',
+            'imagenes.ruta as imagen_producto',
+            'imagenes.id as imagen_id'
         )
         ->join('categorias', 'productos.categoria_id', '=' , 'categorias.id')
         ->join('proveedores', 'productos.proveedor_id', '=' , 'proveedores.id')
+        ->leftJoin('imagenes', 'productos.id', '=', 'imagenes.producto_id')
         ->get();
 
         return view('modules.reportes_productos.index', compact('titulo', 'items'));
