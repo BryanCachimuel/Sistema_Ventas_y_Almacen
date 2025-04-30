6333ffbe272e20244ac55d92c19bc57432d9fcfc
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-29 23:01:10 -0500

    Creando la función agregar carrito la misma que contiene un proceso realizar el proceso de ventas

--- app/Http/Controllers/Ventas.php
+++ app/Http/Controllers/Ventas.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Ventas extends Controller
 {
     public function index(){
         $titulo = "Venta de Productos";
+        $items = Producto::all();
+        return view('modules.ventas.index', compact('titulo','items'));
+    }
+
+    public function agregar_carrito($id_producto)
+    {
+        $titulo = 'Ventas';
+        $item = Producto::find($id_producto);
+
+        // obtener los productos ya almacenados
+        $items_carrito = Session::get('items_carrito', []);
+
+        // agregar el nuevo producto
+        $items_carrito [] = [
+            'id' => $item->id,
+            'nombre' => $item->nombre
+        ];
+
+        // se crea una sesión
+        Session::put('items_carrito', $items_carrito);
+
         $items = Producto::all();
         return view('modules.ventas.index', compact('titulo','items'));
     }
