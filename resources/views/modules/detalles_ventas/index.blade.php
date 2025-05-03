6c27cd0e36f2089b430e135551696681667a5373
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-02 22:31:00 -0500

    Presentando la vista del detalle de venta en donde se lista cada registro realizado de una venta

--- resources/views/modules/detalles_ventas/index.blade.php
+++ resources/views/modules/detalles_ventas/index.blade.php
@@ -1,13 +1,57 @@
 @extends('layouts.main')
 
+@section('titulo', $titulo)
+    
+@section('contenido')
 <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Detalles de Venta</h1>
-    </div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Detalle de Ventas Realizadas</h1>
+  </div><!-- End Page Title -->
 
-    <section class="section dashboard">
-     
-    </section>
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Revisar Ventas existentes</h5>
+      
+            <!-- Table with stripped rows -->
+            <table class="table datatable table-condensed">
+              <thead>
+                <tr>
+                 <th class="text-center">Total Vendido</th>
+                 <th class="text-center">Fecha Venta</th>
+                 <th class="text-center">Usuario</th>
+                 <th class="text-center">Ver Detalle</th>
+                 <th class="text-center">Revocar Venta</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($items as $item)
+                <tr>
+                  <td class="text-center">${{ $item->total_venta }}</td>
+                  <td class="text-center">{{ $item->created_at }}</td>
+                  <td class="text-center">{{ $item->nombre_usuario }}</td>
+                  <td class="text-center">
+                    <a href="" class="btn btn-info">Detalle</a>
+                  </td>
+                  <td class="text-center">
+                    <a href="" class="btn btn-danger">Revocar Venta</a>
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 
 </main>
+@endsection
