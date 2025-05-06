65511291476cc8a19ab5b2bab46f4795e5ba4d5f
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-05 22:03:39 -0500

    Realizando el diseño con css para la impresión del ticket de compra

--- resources/views/modules/detalles_ventas/ticket.blade.php
+++ resources/views/modules/detalles_ventas/ticket.blade.php
@@ -5,10 +5,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ticket de Compra</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .ticket{
+            width: 400px;
+            margin: auto;
+            padding: 10px;
+            border: 1px solid #000;
+        }
+        .titulo{
+            font-size: 18px;
+            font-weight: bold;
+            text-align: center;
+            color: #ff0000;
+        }
+        .detalle{
+            text-align: left;
+            margin-top: 10px;
+        }
+        .total{
+            font-size: 16px;
+            font-weight: bold;
+            margin-top: 10px;
+            margin-left: 210px;
+        }
+        .footer{
+            margin-top: 20px;
+            font-weight: bold;
+            color: #0000ff;
+            text-align: center;
+        }
+        table{
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td{
+            border-bottom: 1px solid #000;
+            padding: 5px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
     <div class="ticket">
-        <p>Ticket de Compra de Ventas y Almacén</p>
+        <p class="titulo">Ticket de Compra de Ventas y Almacén</p>
         <p>
             <strong>Cajero: </strong> {{ $venta->nombre_usuario }}
         </p>
@@ -38,10 +80,10 @@
                 </tbody>
             </table>
         </div>
-        <p>
+        <p class="total">
             <strong>Total de venta: </strong> ${{ $venta->total_venta }}
         </p>
-        <p>Gracias por su Compra realizada</p>
+        <p class="footer">!!!!!Gracias por su Compra realizada!!!!!</p>
     </div>
 </body>
 </html>
