7f4631da90ee558ae5cf9b0ec3485bdd8214debf
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-06 20:12:57 -0500

    Reestructurando la vista de dashboard

--- resources/views/modules/dashboard/home.blade.php
+++ resources/views/modules/dashboard/home.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts.main')
+
+@section('titulo', $titulo)
+    
+@section('contenido')
 <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Dashboard</h1>
+  </div><!-- End Page Title -->
 
-    <section class="section dashboard">
-     
-    </section>
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
 
-  </main>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Datos del Sistema de Ventas y Almacen</h5>
+           
+           
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+@endsection
+
