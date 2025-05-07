dd0092ff7287a89c9adba7758ef2388e81dbfb41
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-06 20:53:06 -0500

    Agregando el diseño de tarjetas para últimas ventas y productos con menor stock

--- resources/views/modules/dashboard/home.blade.php
+++ resources/views/modules/dashboard/home.blade.php
@@ -15,7 +15,7 @@
 
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Bienvenido, {{ Auth::user()->name }}</h5>
+            <h3 class="mt-3 mb-4">Bienvenido, {{ Auth::user()->name }}</h3>
            
             <div class="row">
               <div class="col-1"></div>
@@ -23,7 +23,7 @@
                 <div class="card w-50 bg-primary">
                   <div class="card-body">
                     <h5 class="card-title text-white text-center">Total de ventas</h5>
-                    <p class="card-text text-white text-center">${{ number_format($totalVentas, 2) }}</p>
+                    <h2 class="card-text text-white text-center">${{ number_format($totalVentas, 2) }}</h2>
                   </div>
                 </div>
               </div>
@@ -34,7 +34,7 @@
                 <div class="card w-50 bg-success">
                   <div class="card-body">
                     <h5 class="card-title text-white text-center">Cantidad de ventas</h5>
-                    <p class="card-text text-white text-center">{{ $cantidadVentas }}</p>
+                    <h2 class="card-text text-white text-center">{{ $cantidadVentas }}</h2>
                   </div>
                 </div>
               </div>
@@ -42,16 +42,29 @@
             </div>
 
             <div class="row mt-3">
-              <div class="col">
-                <h3>Últimas ventas:</h3>
-                <ul>
-                  @foreach ($ventasRecientes as $item)
-                      <li>Venta #{{ $item->id }} - ${{ number_format($item->total_venta, 2) }}</li>
-                  @endforeach
-                </ul>
+              <div class="col-1"></div>
+              <div class="col-5">
+                <div class="card w-50 bg-info">
+                  <div class="card-body">
+                    <h5 class="card-title text-white text-center">Últimas ventas</h5>
+                    <h3 class="card-text text-white text-center">
+                        @foreach ($ventasRecientes as $item)
+                            Venta #{{ $item->id }} - ${{ number_format($item->total_venta, 2) }}
+                        @endforeach
+                    </h3>
+                  </div>
+                </div>
               </div>
-              <div class="col">
-                <h4>Productos con Bajo Stock: {{ count($productoBajosStock) }}</h4>
+
+              <div class="col-1"></div>
+
+              <div class="col-5">
+                <div class="card w-50 bg-warning">
+                  <div class="card-body">
+                    <h5 class="card-title text-white text-center">Productos con Menor Stock</h5>
+                    <h2 class="card-text text-white text-center">{{ count($productoBajosStock) }}</h2>
+                  </div>
+                </div>
               </div>
             </div>
 
