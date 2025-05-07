db18dd6f3886086d7df1b2f2f84f09c0bc8d63fa
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-06 21:03:04 -0500

    Adecuando las tarjetas para obtener los nuevos valores de proveedores y productos para el dashboard

--- resources/views/modules/dashboard/home.blade.php
+++ resources/views/modules/dashboard/home.blade.php
@@ -18,9 +18,9 @@
             <h3 class="mt-3 mb-4">Bienvenido, {{ Auth::user()->name }}</h3>
            
             <div class="row">
-              <div class="col-1"></div>
-              <div class="col-5">
-                <div class="card w-50 bg-primary">
+
+              <div class="col-4">
+                <div class="card w-55 bg-primary">
                   <div class="card-body">
                     <h5 class="card-title text-white text-center">Total de ventas</h5>
                     <h2 class="card-text text-white text-center">${{ number_format($totalVentas, 2) }}</h2>
@@ -28,10 +28,17 @@
                 </div>
               </div>
 
-              <div class="col-1"></div>
+              <div class="col-4">
+                <div class="card w-55 bg-danger">
+                  <div class="card-body">
+                    <h5 class="card-title text-white text-center">Total de Productos</h5>
+                    <h2 class="card-text text-white text-center">{{ $cantidadProductos}}</h2>
+                  </div>
+                </div>
+              </div>
 
-              <div class="col-5">
-                <div class="card w-50 bg-success">
+              <div class="col-4">
+                <div class="card w-55 bg-success">
                   <div class="card-body">
                     <h5 class="card-title text-white text-center">Cantidad de ventas</h5>
                     <h2 class="card-text text-white text-center">{{ $cantidadVentas }}</h2>
@@ -42,9 +49,8 @@
             </div>
 
             <div class="row mt-3">
-              <div class="col-1"></div>
-              <div class="col-5">
-                <div class="card w-50 bg-info">
+              <div class="col-4">
+                <div class="card w-55 bg-info">
                   <div class="card-body">
                     <h5 class="card-title text-white text-center">Últimas ventas</h5>
                     <h3 class="card-text text-white text-center">
@@ -56,10 +62,17 @@
                 </div>
               </div>
 
-              <div class="col-1"></div>
+              <div class="col-4">
+                <div class="card w-55 bg-secondary">
+                  <div class="card-body">
+                    <h5 class="card-title text-white text-center">Total Proveedores</h5>
+                    <h2 class="card-text text-white text-center">{{ $cantidadProveedores }}</h2>
+                  </div>
+                </div>
+              </div>
 
-              <div class="col-5">
-                <div class="card w-50 bg-warning">
+              <div class="col-4">
+                <div class="card w-55 bg-warning">
                   <div class="card-body">
                     <h5 class="card-title text-white text-center">Productos con Menor Stock</h5>
                     <h2 class="card-text text-white text-center">{{ count($productoBajosStock) }}</h2>
