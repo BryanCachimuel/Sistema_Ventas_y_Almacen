f7575873f0d2b29ece3af3d7d51917090052c0dc
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-06 20:28:15 -0500

    Obteniendo desde el controlador los valores para tarjetas de total de ventas, cantidad de ventas, productos con stock bajo y última venta

--- resources/views/modules/dashboard/home.blade.php
+++ resources/views/modules/dashboard/home.blade.php
@@ -15,9 +15,30 @@
 
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Datos del Sistema de Ventas y Almacen</h5>
+            <h5 class="card-title">Bienvenido, {{ Auth::user()->name }}</h5>
            
-           
+            <div class="row">
+              <div class="col">
+                <h4>Total de ventas: ${{ number_format($totalVentas, 2) }}</h4>
+              </div>
+              <div class="col">
+                <h4>Cantidad de ventas: {{ $cantidadVentas }}</h4>
+              </div>
+              <div class="col">
+                <h4>Productos con Bajo Stock: {{ count($productoBajosStock) }}</h4>
+              </div>
+            </div>
+
+            <div class="row mt-3">
+              <div class="col">
+                <h3>Últimas ventas:</h3>
+                <ul>
+                  @foreach ($ventasRecientes as $item)
+                      <li>Venta #{{ $item->id }} - ${{ number_format($item->total_venta, 2) }}</li>
+                  @endforeach
+                </ul>
+              </div>
+            </div>
 
           </div>
         </div>
