d60e8311a3332baafac04c47ce9b453cab1282c9
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-06 20:41:01 -0500

    Arreglando la tarjeta para Total ventas y cantidad de ventas

--- resources/views/modules/dashboard/home.blade.php
+++ resources/views/modules/dashboard/home.blade.php
@@ -18,15 +18,27 @@
             <h5 class="card-title">Bienvenido, {{ Auth::user()->name }}</h5>
            
             <div class="row">
-              <div class="col">
-                <h4>Total de ventas: ${{ number_format($totalVentas, 2) }}</h4>
+              <div class="col-1"></div>
+              <div class="col-5">
+                <div class="card w-50 bg-primary">
+                  <div class="card-body">
+                    <h5 class="card-title text-white text-center">Total de ventas</h5>
+                    <p class="card-text text-white text-center">${{ number_format($totalVentas, 2) }}</p>
+                  </div>
+                </div>
               </div>
-              <div class="col">
-                <h4>Cantidad de ventas: {{ $cantidadVentas }}</h4>
+
+              <div class="col-1"></div>
+
+              <div class="col-5">
+                <div class="card w-50 bg-success">
+                  <div class="card-body">
+                    <h5 class="card-title text-white text-center">Cantidad de ventas</h5>
+                    <p class="card-text text-white text-center">{{ $cantidadVentas }}</p>
+                  </div>
+                </div>
               </div>
-              <div class="col">
-                <h4>Productos con Bajo Stock: {{ count($productoBajosStock) }}</h4>
-              </div>
+              
             </div>
 
             <div class="row mt-3">
@@ -37,6 +49,9 @@
                       <li>Venta #{{ $item->id }} - ${{ number_format($item->total_venta, 2) }}</li>
                   @endforeach
                 </ul>
+              </div>
+              <div class="col">
+                <h4>Productos con Bajo Stock: {{ count($productoBajosStock) }}</h4>
               </div>
             </div>
 
