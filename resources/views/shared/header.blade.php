fbb3f35ca4ab1d7ad0c2dc4d1f065a9d36e7e7e6
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-07 22:33:31 -0500

    Añadiendo adecuadamente el rol del usuario en la parte del header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -29,7 +29,13 @@
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
               <h6>{{ Auth::user()->name }}</h6>
-              <span>{{ Auth::user()->rol }}</span>
+              <span>
+                @if (Auth::user()->rol == 'admin')
+                    Administrador
+                @else
+                    Cajero
+                @endif
+              </span>
             </li>
             <li>
               <hr class="dropdown-divider">
