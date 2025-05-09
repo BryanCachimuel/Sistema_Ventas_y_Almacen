59a698b14c13d653a5828c2c1e4d463ac3526fb3
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-08 22:05:04 -0500

    Adecuando los labels e inputs del formulario de creación de usuarios

--- resources/views/modules/usuarios/create.blade.php
+++ resources/views/modules/usuarios/create.blade.php
@@ -17,21 +17,38 @@
             
             <form action="{{ route("usuarios.store") }}" method="POST">
                 @csrf
-                <label for="name">Nombre de Usuario</label>
-                <input type="text" class="form-control" name="name" id="name" required>
 
-                <label for="email">Correo</label>
-                <input type="email" class="form-control" name="email" id="email" required>
+                <div class="form-group row mb-2">
+                  <label for="name" class="col-sm-2 col-form-label">Nombre de Usuario: </label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="text" class="form-control" name="name" id="name" required>
+                  </div>
+                </div>
+                
+                <div class="form-group row">
+                  <label for="email" class="col-sm-2 col-form-label">Correo de Usuario: </label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="email" class="form-control" name="email" id="email" required>
+                  </div>
+                </div>
 
-                <label for="password">Contraseña</label>
-                <input type="password" class="form-control" name="password" id="password" required>
-
-                <label for="rol">Rol de Usuario</label>
-                <select name="rol" id="rol" class="form-select">
-                    <option value="">Selecciona el Rol</option>
-                    <option value="admin">Administrador</option>
-                    <option value="cajero">Cajero</option>
-                </select>
+                <div class="form-group row mt-2">
+                  <label for="password" class="col-sm-2 col-form-label">Contraseña: </label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="password" class="form-control" name="password" id="password" required>
+                  </div>
+                </div>
+                
+                <div class="form-group row mt-2">
+                  <label for="rol" class="col-sm-2 col-form-label">Rol de Usuario: </label>
+                  <div class="col-sm-10 col-form-label">
+                    <select name="rol" id="rol" class="form-select">
+                      <option value="">Seleccione un Rol</option>
+                      <option value="admin">Administrador</option>
+                      <option value="cajero">Cajero</option>
+                  </select>
+                  </div>
+                </div>
 
                 <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
                 <a href="{{ route("usuarios") }}" class="btn btn-outline-danger mt-3">
