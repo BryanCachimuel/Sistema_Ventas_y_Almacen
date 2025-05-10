93bc1c27169faf099cf19271e6235905da5701df
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-09 23:15:22 -0500

    Moviendo los botones de eliminación y cancelar en la sección de eliminación de categorías

--- resources/views/modules/categorias/show.blade.php
+++ resources/views/modules/categorias/show.blade.php
@@ -18,12 +18,22 @@
             <form action="{{ route("categorias.destroy", $item->id) }}" method="POST">
                 @csrf
                 @method('DELETE')
-                <label for="nombre">Nombre de categoria</label>
-                <input type="text" class="form-control" readonly name="nombre" id="nombre" value="{{ $item->nombre }}">
-                <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-trash-can"></i> Eliminar</button>
-                <a href="{{ route("categorias") }}" class="btn btn-outline-danger mt-3">
-                  <i class="fa-solid fa-circle-xmark"></i> Cancelar
-                </a>
+
+                <div class="form-group row mb-2">
+                  <label for="nombre" class="col-sm-2 col-form-label">Nombre:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="text" class="form-control" readonly name="nombre" id="nombre" value="{{ $item->nombre }}">
+                  </div>
+                </div>
+
+                <div class="text-end">
+                  <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-trash-can"></i> Eliminar</button>
+                  <a href="{{ route("categorias") }}" class="btn btn-outline-danger mt-3">
+                    <i class="fa-solid fa-circle-xmark"></i> Cancelar
+                  </a>
+                </div>
+                
+                
             </form>
           </div>
         </div>
