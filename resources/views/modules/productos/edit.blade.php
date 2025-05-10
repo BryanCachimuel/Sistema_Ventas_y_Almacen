b452d05c0905b2bee8827c55bb6ee34fe4f6fe4f
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-09 23:07:04 -0500

    Mejorando los label e inputs de la sección de actualización de productos

--- resources/views/modules/productos/edit.blade.php
+++ resources/views/modules/productos/edit.blade.php
@@ -18,48 +18,74 @@
             <form action="{{ route('productos.update', $item->id) }}" method="POST">
                 @csrf
                 @method('PUT')
-                <label for="categoria_id">Categoría</label>
-                <select name="categoria_id" id="categoria_id" class="form-select" required>
-                    <option value="">Selecciona una categoría</option>
-                    @foreach ($categorias as $categoria)
-                        @if ($item->categoria_id == $categoria->id)
-                            <option selected value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
-                        @else
-                            <option value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
-                        @endif
 
-                    @endforeach
-                </select>
+                <div class="form-group row mb-2">
+                  <label for="categoria_id" class="col-sm-2 col-form-label">Categoría:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <select name="categoria_id" id="categoria_id" class="form-select" required>
+                      <option value="">Selecciona una categoría</option>
+                      @foreach ($categorias as $categoria)
+                          @if ($item->categoria_id == $categoria->id)
+                              <option selected value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
+                          @else
+                              <option value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
+                          @endif
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
 
-                <label for="proveedor_id">Proveedor</label>
-                <select name="proveedor_id" id="proveedor_id" class="form-select" required>
-                    <option value="">Selecciona un proveedor</option>
-                    @foreach ($proveedores as $proveedor)
-                        @if ($item->proveedor_id == $proveedor->id)
-                            <option selected value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
-                        @else
-                            <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
-                        @endif
-                        
-                    @endforeach
-                </select>
+                <div class="form-group row mb-2">
+                  <label for="proveedor_id" class="col-sm-2 col-form-label">Proveedor:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <select name="proveedor_id" id="proveedor_id" class="form-select" required>
+                      <option value="">Selecciona un proveedor</option>
+                      @foreach ($proveedores as $proveedor)
+                          @if ($item->proveedor_id == $proveedor->id)
+                              <option selected value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
+                          @else
+                              <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
+                          @endif 
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
+                
+                <div class="form-group row mb-2">
+                  <label for="codigo" class="col-sm-2 col-form-label">Código de producto:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="text" class="form-control" name="codigo" id="codigo" value="{{ $item->codigo }}" required>
+                  </div>
+                </div>
+                
+                <div class="form-group row mb-2">
+                  <label for="nombre" class="col-sm-2 col-form-label">Nombre:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="text" class="form-control" name="nombre" id="nombre" value="{{ $item->nombre }}" required>
+                  </div>
+                </div>
 
-                <label for="codigo">Código de producto</label>
-                <input type="text" class="form-control" name="codigo" id="codigo" value="{{ $item->codigo }}" required>
+                <div class="form-group row mb-2">
+                  <label for="descripcion" class="col-sm-2 col-form-label">Descripción:</label>
+                  <div class="col-sm-10 col-form-label">
+                     <textarea class="form-control" name="descripcion" id="descripcion" cols="20" rows="2">{{ $item->descripcion }}</textarea>
+                  </div>
+                </div>
 
-                <label for="nombre">Nombre de producto</label>
-                <input type="text" class="form-control" name="nombre" id="nombre" value="{{ $item->nombre }}" required>
-
-                <label for="descripcion">Descripción</label>
-                <textarea class="form-control" name="descripcion" id="descripcion" cols="20" rows="5">{{ $item->descripcion }}</textarea>
-
-                <label for="precio_venta">Precio de Venta</label>
-                <input type="text" name="precio_venta" id="precio_venta" class="form-control" value="{{ $item->precio_venta }}" required>
-
-                <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-pen-to-square"></i> Actualizar</button>
-                <a href="{{ route("productos") }}" class="btn btn-outline-danger mt-3">
-                  <i class="fa-solid fa-circle-xmark"></i> Cancelar
-                </a>
+                <div class="form-group row mb-2">
+                  <label for="precio_venta" class="col-sm-2 col-form-label">Precio de Venta:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="text" name="precio_venta" id="precio_venta" class="form-control" value="{{ $item->precio_venta }}" required>
+                  </div>
+                </div>
+                
+                <div class="text-end">
+                  <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-pen-to-square"></i> Actualizar</button>
+                  <a href="{{ route("productos") }}" class="btn btn-outline-danger mt-3">
+                    <i class="fa-solid fa-circle-xmark"></i> Cancelar
+                  </a>
+                </div>
+              
             </form>
           </div>
         </div>
