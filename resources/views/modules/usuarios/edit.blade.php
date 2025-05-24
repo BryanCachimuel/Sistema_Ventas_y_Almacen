9a1a35cea66485f8c0ed42b117809da2cdb1c0b6
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-23 22:24:50 -0500

    Mejorando la vista del formulario de actualización de usuarios en especial de los labels, inputs y buttons

--- resources/views/modules/usuarios/edit.blade.php
+++ resources/views/modules/usuarios/edit.blade.php
@@ -18,28 +18,42 @@
             <form action="{{ route("usuarios.update", $item->id) }}" method="POST" autocomplete="off">
                 @csrf
                 @method("PUT")
-                <label for="name">Nombre de Usuario</label>
-                <input type="text" class="form-control" name="name" id="name" value="{{ $item->name }}" required>
+    
+                <div class="form-group row mb-2">
+                  <label for="name" class="col-sm-2 col-form-label">Nombre:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="text" class="form-control" name="name" id="name" value="{{ $item->name }}" required>
+                  </div>
+                </div>
 
-                <label for="email">Correo</label>
-                <input type="email" class="form-control" name="email" id="email" value="{{ $item->email }}" required>
+                <div class="form-group row mb-2">
+                  <label for="email" class="col-sm-2 col-form-label">Correo:</label>
+                  <div class="col-sm-10 col-form-label">
+                    <input type="email" class="form-control"name="email" id="email" value="{{ $item->email }}" required>
+                  </div>
+                </div>
 
-                <label for="rol">Rol de Usuario</label>
-                <select name="rol" id="rol" class="form-select">
-                    @if ($item->rol == 'admin')
+                <div class="form-group row mt-2">
+                  <label for="rol" class="col-sm-2 col-form-label">Rol de Usuario: </label>
+                  <div class="col-sm-10 col-form-label">
+                    <select name="rol" id="rol" class="form-select">
+                      @if ($item->rol == 'admin')
                         <option value="admin" selected>Administrador</option>
                         <option value="cajero">Cajero</option>
-                    @else
-                        <option value="admin">Administrador</option>
-                        <option value="cajero" selected>Cajero</option>
-                    @endif
-                   
-                    
-                </select>
-                <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-pen-to-square"></i> Actualizar</button>
-                <a href="{{ route("usuarios") }}" class="btn btn-outline-danger mt-3">
-                  <i class="fa-solid fa-circle-xmark"></i> Cancelar
-                </a>
+                      @else
+                          <option value="admin">Administrador</option>
+                          <option value="cajero" selected>Cajero</option>
+                      @endif
+                  </select>
+                  </div>
+                </div>
+
+                <div class="text-end">
+                  <button class="btn btn-outline-primary mt-3"><i class="fa-solid fa-pen-to-square"></i> Actualizar</button>
+                  <a href="{{ route("usuarios") }}" class="btn btn-outline-danger mt-3">
+                    <i class="fa-solid fa-circle-xmark"></i> Cancelar
+                  </a>
+                </div>
             </form>
           </div>
         </div>
